afaf4c8b6d00fbc8a0c1a689c48265e48c127838
kekaiyuan <1054623598@qq.com>
2021-06-17 00:36:27 +0800

    策略模式

---
+++ src/com/kky/dp/singleton/src/com/kky/Mgr05.java
@@ -0,0 +1,41 @@
+package com.kky.dp.singleton.src.com.kky;
+
+/**
+ * @author 柯凯元
+ * @create 2021/6/2 22:13
+ */
+/*
+* Mgr04的改进，尝试减小同步代码块，结果不可行
+* 注意：该代码不可行！
+* */
+public class Mgr05 {
+    private static Mgr05 INSTANCE;
+
+    private Mgr05(){
+
+    }
+
+    public static Mgr05 getInstance() {
+        if(INSTANCE == null){
+            //该方法不可行！！!
+            synchronized (Mgr05.class){
+                try{
+                    Thread.sleep(1);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                INSTANCE = new Mgr05();
+            }
+
+        }
+        return INSTANCE;
+    }
+
+    public static void main(String[] args) {
+        //多线程访问，通过hashcode检验是否是单例模式
+        for(int i=0;i<100;i++){
+            new Thread(()-> System.out.println(Mgr05.getInstance().hashCode())).start();
+        }
+    }
+}
+
---
+++ src/com/kky/dp/singleton/src/com/kky/Mgr08.java
@@ -0,0 +1,16 @@
+package com.kky.dp.singleton.src.com.kky;
+
+/**
+ * @author 柯凯元
+ * @create 2021/6/2 22:14
+ */
+/*
+* 枚举方式
+* 不仅可以解决线程同步，还可以防止反序列化
+* 完美版本3
+* */
+public enum Mgr08 {
+
+    INSTANCE;
+
+}
