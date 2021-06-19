f6469b004741370cac1055cd5c36705527d8b048
kekaiyuan <1054623598@qq.com>
2021-06-20 03:08:44 +0800

    策略模式

---
+++ src/main/java/com/kky/dp/strategy/Comparator.java
@@ -0,0 +1,11 @@
+package com.kky.dp.strategy;
+
+/**
+ * @author 柯凯元
+ * @create 2021/6/13 16:47
+ */
+public abstract class Comparator<T> {
+
+    public abstract int compare(T o1, T o2);
+
+}
---
+++ src/main/java/com/kky/dp/strategy/ComparatorContext.java
@@ -0,0 +1,21 @@
+package com.kky.dp.strategy;
+
+/**
+ * @author 柯凯元
+ * @create 2021/6/13 4:34
+ */
+
+//Context上下文角色，也叫Context封装角色。
+//起承上启下的作用，屏蔽高层模块对策略、算法的直接访问，封装可能存在的变化。
+public class ComparatorContext<T> {
+    private Comparator comparator;
+
+    public ComparatorContext(Comparator comparator) {
+        this.comparator = comparator;
+    }
+
+    public int compare(T o1, T o2){
+        return comparator.compare(o1, o2);
+    }
+
+}
