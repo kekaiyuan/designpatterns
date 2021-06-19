cecaf75552de0274ac44259a44e8915117e9559d
kekaiyuan <1054623598@qq.com>
2021-06-20 03:33:29 +0800

    迭代器

---
+++ src/main/java/com/kky/dp/Iterator/v3/Collection_.java
@@ -0,0 +1,6 @@
+package com.kky.dp.Iterator.v3;
+
+public interface Collection_ {
+    void add(Object o);
+    int size();
+}
---
+++ src/main/java/com/kky/dp/Iterator/v5/Iterator_.java
@@ -0,0 +1,7 @@
+package com.kky.dp.Iterator.v5;
+
+public interface Iterator_ {
+    boolean hasNext();
+
+    Object next();
+}
